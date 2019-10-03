b0a65946185bd44761b4e1c9d246a6c4707dd379
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-03 20:59:37 +0300

    This commit creates categories and location models

--- photos/models.py
+++ photos/models.py
@@ -1,11 +1,36 @@
 from django.db import models
 
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delte_location(self):
+        self.delete()
+
+class categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_categories(self):
+        self.save()
+
+    def delete_categories(self):
+        self.delete()    
+
+
 class Image(models.Model):
     image = models.ImageField(default='default.jpg' upload_to = 'pictures/')
     image_name = models.CharField(max_length=30,default="Title")
     image_description = models.TextField()
     image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    image_category = models.ManyToManyField(category)
+    image_categories = models.ManyToManyField(categories)
 
     def __str__(self):
         return self.image_name
