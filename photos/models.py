7046e4e87a36ced79c58ebcf68464c6f517e158f
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-03 20:43:49 +0300

    This commit adds image model and create its dunda function

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(default='default.jpg' upload_to = 'pictures/')
+    image_name = models.CharField(max_length=30,default="Title")
+    image_description = models.TextField()
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ManyToManyField(category)
+
+    def __str__(self):
+        return self.image_name
