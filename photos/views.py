783d1031ad7528c2fbc20f9b0328801c6a11fd2b
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-04 08:40:11 +0300

    This commit adds a view function to get location and images based on loactions

--- photos/views.py
+++ photos/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
 
+def location (request, location):
+    locations = Location.objects.all()
+    chosen_location = Location.objects.get(id=location)
+    images = Image.objects.filter(location=chosen_location.id)
+    context = {
+        'location':chosen_location, 'locations':locations, 'images':images
+    }
+    return render(request, 'location.html',context)
