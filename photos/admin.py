929b27f5f702c5b8fd0ac22c97e4e5e035c97d20
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-04 13:01:42 +0300

    This commit adds models to django admin, modifies settings to display images, adds content to images template and modifies location template

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Image, Location, categories
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('image_categories',)
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Location)
+admin.site.register(categories,)
--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     url(r'^$', views.get_images, name='images'),
     url(r'^location/(\d+)', views.get_location, name='location'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
