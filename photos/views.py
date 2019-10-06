14c1f164f1670e87ae31e3afe454ce6253b4d95f
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-06 16:15:37 +0300

    -m"This commit renders search template in search_resluts view function"

--- photos/views.py
+++ photos/views.py
@@ -21,6 +21,10 @@ def get_location (request, location):
     return render(request, 'location.html',context)
 
 def seach_results(request):
-    if category in request.GET and request.GET['category']:
+    if 'category' in request.GET and request.GET['category']:
         search_term = request.GET.get('category')
         searched_images = Image.search_by_category((search_term))
+        message = f"{search_term}"
+        return render(request, 'search.html',{"message":message,"images":searched_images,"category":search_term})
+    else:
+        return render(request, 'search.html',{"message":message})
