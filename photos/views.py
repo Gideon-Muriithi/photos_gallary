fccd30730d6e4118307162b0f7dc2b4fca990842
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-07 08:44:00 +0300

    Rename referenced attribute to match one in image model

--- photos/views.py
+++ photos/views.py
@@ -14,7 +14,7 @@ def get_images(request):
 def get_location (request, location):
     locations = Location.objects.all()
     chosen_location = Location.objects.get(id=location)
-    images = Image.objects.filter(location=chosen_location.id)
+    images = Image.objects.filter(image_location=chosen_location.id)
     context = {
         'location':chosen_location, 'locations':locations, 'images':images
     }
